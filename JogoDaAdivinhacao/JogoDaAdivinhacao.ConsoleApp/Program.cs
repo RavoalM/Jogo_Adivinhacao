521955b13b607b07e08887701b2403800880f79b
RavoalM <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-12 14:34:42 -0300

    Jogo de adivinhação

--- JogoDaAdivinhacao/JogoDaAdivinhacao.ConsoleApp/Program.cs
+++ JogoDaAdivinhacao/JogoDaAdivinhacao.ConsoleApp/Program.cs
@@ -15,8 +15,8 @@
                 Console.WriteLine("\nEscolha a dificuldade:");
                 Console.WriteLine("=======================================");
                 Console.WriteLine("1 - Fraco ):< (10 tentativas)");
-                Console.WriteLine("2 - Médio (: (10 tentativas)");
-                Console.WriteLine("3 - Dificil (:< (10 tentativas)");
+                Console.WriteLine("2 - Médio (: (5 tentativas)");
+                Console.WriteLine("3 - Dificil (:< (3 tentativas)");
                 Console.WriteLine("=======================================");
 
                 int totalDeTentativas = 0;
@@ -26,49 +26,57 @@
 
                 if (dificuldade == "1")
                 {
-                    Console.WriteLine("Dificuldade Fraca selecionada");
+                    Console.WriteLine("\nDificuldade Fraca selecionada");
+                    totalDeTentativas = 10;
                 }
                 else if (dificuldade == "2")
                 {
-                    Console.WriteLine("Dificuldade Média selecionada");
+                    Console.WriteLine("\nDificuldade Média selecionada");
+                    totalDeTentativas = 5;
                 }
                 else if (dificuldade == "3")
                 {
-                    Console.WriteLine("Dificuldade Difícil selecionada");
+                    Console.WriteLine("\nDificuldade Difícil selecionada");
+                    totalDeTentativas = 3;
                 }
                 else
                 {
-                    Console.WriteLine("Meu bem você arrumo errado vai lá e faz denovo (hehe esse dai é dos burro)");
+                    Console.WriteLine("\nMeu consagrado você arrumo errado vai lá e faz denovo");
+                    Console.WriteLine("Aperte ENTER para continuar");
                     Console.ReadLine();
                     continue;
                 }
 
+                //geração do numero secreto
                 Random geradorNumeros = new Random();
-                int numeroSecreto = geradorNumeros.Next(1, 20);
+                int numeroSecreto = geradorNumeros.Next(1, 21);
+
+                //loop tentativa
                 for (int tentativas = 1; tentativas <= totalDeTentativas; tentativas++)
                 {
+                    Console.Clear();
                     Console.WriteLine("==========================");
                     Console.WriteLine($"Tentativas {tentativas} de {totalDeTentativas}");
                     Console.WriteLine("==========================");
 
-                    if (tentativas == totalDeTentativas)
-                    {
-                        Console.WriteLine("==========================");
-                        Console.WriteLine($"Mas que peninha, parece que alguem vai perder o sistema operacional, o número era {numeroSecreto}");
-                        Console.WriteLine("Mas sou de coração bom, quer outra chance ???");
-                        Console.WriteLine("==========================");
-
-                        break;
-                    }
-
-                    Console.WriteLine("\n\nDigite um número: ");
+                    Console.WriteLine("\nDigite um número entre 1 e 20 e veja se ganha ");
                     int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
 
                     if (numeroDigitado == numeroSecreto)
                     {
-                        Console.WriteLine("==========================");
+                        Console.WriteLine("\n==========================");
                         Console.WriteLine("Parabéns, você acertou e vai ganhar nada (:");
+                        Console.WriteLine("==========================");
+
+                        break;
+                    }
+
+                    if (tentativas == totalDeTentativas)
+                    {
+                        Console.WriteLine("\n==========================");
+                        Console.WriteLine($"Mas que peninha, parece que alguem vai sair na divida, o número era {numeroSecreto}");
+                        Console.WriteLine("Mas você chegou perto de ganhar o GRANDE PRÊMIO quer tentar denovo???");
                         Console.WriteLine("==========================");
 
                         break;
@@ -76,18 +84,20 @@
 
                     else if (numeroDigitado > numeroSecreto)
                     {
-                        Console.WriteLine("==========================");
+                        Console.WriteLine("\n==========================");
                         Console.WriteLine("Ta indo muito alto em!!!");
                         Console.WriteLine("==========================");
                     }
 
                     else if (numeroDigitado < numeroSecreto)
                     {
-                        Console.WriteLine("==========================");
+                        Console.WriteLine("\n==========================");
                         Console.WriteLine("Ta indo muito baixo em!!!");
                         Console.WriteLine("==========================");
                     }
 
+                    Console.WriteLine("Aperte ENTER para continuar");
+                    Console.ReadLine();
 
                 }
 
@@ -95,10 +105,9 @@
                 string opcaoContinuar = Console.ReadLine().ToUpper();
 
                 if (opcaoContinuar != "S")
-                {
                     break;
-                }
-            }
+                
+            }   
         }
     }
 }
