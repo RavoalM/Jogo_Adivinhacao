e8d3febf62a79f2fe4d852a88b044bf71034de1c
Alvaro Machado <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-18 18:09:15 -0300

    Refatoração

---
+++ JogoDaAdivinhacao/JogoDaAdivinhacao.ConsoleApp/Adivinhacao.cs
@@ -0,0 +1,35 @@
+﻿namespace JogoDaAdivinhacao.ConsoleApp
+{
+    class Adivinhacao
+    {
+        public static bool FacilEscolhida(string dificuldade)
+        {
+            bool FacilEscolhida = dificuldade == "1";
+
+            return FacilEscolhida;
+
+        }
+        public static bool MedioEscolhida(string dificuldade)
+        {
+            bool FacilEscolhida = dificuldade == "2";
+
+            return FacilEscolhida;
+
+        }
+        public static bool DificilEscolhida(string dificuldade)
+        {
+            bool FacilEscolhida = dificuldade == "2";
+
+            return FacilEscolhida;
+
+        }
+
+        public static int GeradorNumero()
+        {
+            Random geradorNumeros = new Random();
+            int numeroSecreto = geradorNumeros.Next(1, 21);
+
+            return numeroSecreto;
+        }
+    }
+}
--- JogoDaAdivinhacao/JogoDaAdivinhacao.ConsoleApp/Program.cs
+++ JogoDaAdivinhacao/JogoDaAdivinhacao.ConsoleApp/Program.cs
@@ -1,99 +1,64 @@
-﻿namespace JogoDeAdivinhacao.ConsoleApp
+﻿using JogoDaAdivinhacao.ConsoleApp;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+
+namespace JogoDeAdivinhacao.ConsoleApp
 {
     internal class Program
     {
+        static int totalDeTentativas = 0;
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Jogo de Adivinhação");
-                Console.WriteLine("=======================================");
-                Console.WriteLine("\nSeja muito bem vindo ao novo jogo do Largartinho");
+                string dificuldade = ExibirMenu();
 
-                //menu
-                Console.WriteLine("\nEscolha a dificuldade:");
-                Console.WriteLine("=======================================");
-                Console.WriteLine("1 - Fraco ):< (10 tentativas)");
-                Console.WriteLine("2 - Médio (: (5 tentativas)");
-                Console.WriteLine("3 - Dificil (:< (3 tentativas)");
-                Console.WriteLine("=======================================");
-
-                int totalDeTentativas = 0;
-
-                Console.WriteLine("Digite a dificuldade desejada: ");
-                string dificuldade = Console.ReadLine();
-
-                if (dificuldade == "1")
+                if (Adivinhacao.FacilEscolhida(dificuldade))
                 {
-                    Console.WriteLine("\nDificuldade Fraca selecionada");
-                    totalDeTentativas = 10;
+                    FacilExibir();
                 }
-                else if (dificuldade == "2")
+                else if (Adivinhacao.MedioEscolhida(dificuldade))
                 {
-                    Console.WriteLine("\nDificuldade Média selecionada");
-                    totalDeTentativas = 5;
+                    MedioExibir();
                 }
-                else if (dificuldade == "3")
+                else if (Adivinhacao.DificilEscolhida(dificuldade))
                 {
-                    Console.WriteLine("\nDificuldade Difícil selecionada");
-                    totalDeTentativas = 3;
+                    DificilExibir();
                 }
                 else
                 {
-                    Console.WriteLine("\nMeu consagrado você arrumo errado vai lá e faz denovo");
-                    Console.WriteLine("Aperte ENTER para continuar");
-                    Console.ReadLine();
+                    FalhaExibir();
                     continue;
                 }
 
                 //geração do numero secreto
-                Random geradorNumeros = new Random();
-                int numeroSecreto = geradorNumeros.Next(1, 21);
+                int numeroSecreto = Adivinhacao.GeradorNumero();
 
                 //loop tentativa
                 for (int tentativas = 1; tentativas <= totalDeTentativas; tentativas++)
                 {
-                    Console.Clear();
-                    Console.WriteLine("==========================");
-                    Console.WriteLine($"Tentativas {tentativas} de {totalDeTentativas}");
-                    Console.WriteLine("==========================");
-
-                    Console.WriteLine("\nDigite um número entre 1 e 20 e veja se ganha ");
-                    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
-
+                    int numeroDigitado = Gameplay(tentativas);
 
                     if (numeroDigitado == numeroSecreto)
                     {
-                        Console.WriteLine("\n==========================");
-                        Console.WriteLine("Parabéns, você acertou e vai ganhar nada (:");
-                        Console.WriteLine("==========================");
-
+                        VitoriaExibir();
                         break;
                     }
 
                     if (tentativas == totalDeTentativas)
                     {
-                        Console.WriteLine("\n==========================");
-                        Console.WriteLine($"Mas que peninha, parece que alguem vai sair na divida, o número era {numeroSecreto}");
-                        Console.WriteLine("Mas você chegou perto de ganhar o GRANDE PRÊMIO quer tentar denovo???");
-                        Console.WriteLine("==========================");
-
+                        DerrotaExibir(numeroSecreto);
                         break;
                     }
 
                     else if (numeroDigitado > numeroSecreto)
                     {
-                        Console.WriteLine("\n==========================");
-                        Console.WriteLine("Ta indo muito alto em!!!");
-                        Console.WriteLine("==========================");
+                        NaltoExibir();
                     }
 
                     else if (numeroDigitado < numeroSecreto)
                     {
-                        Console.WriteLine("\n==========================");
-                        Console.WriteLine("Ta indo muito baixo em!!!");
-                        Console.WriteLine("==========================");
+                        NbaixoExibir();
                     }
 
                     Console.WriteLine("Aperte ENTER para continuar");
@@ -101,13 +66,104 @@
 
                 }
 
-                Console.WriteLine("Deseja continuar? (S/N)");
-                string opcaoContinuar = Console.ReadLine().ToUpper();
+                string opcaoContinuar = mensagemFinal();
 
                 if (opcaoContinuar != "S")
                     break;
-                
-            }   
+
+            }
+
+
+        }
+        public static string ExibirMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Jogo de Adivinhação");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("\nSeja muito bem vindo ao novo jogo do Largartinho");
+
+            //menu
+            Console.WriteLine("\nEscolha a dificuldade:");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("1 - Fraco ):< (10 tentativas)");
+            Console.WriteLine("2 - Médio (: (5 tentativas)");
+            Console.WriteLine("3 - Dificil (:< (3 tentativas)");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("Digite a dificuldade desejada: ");
+            string dificuldade = Console.ReadLine();
+
+            return dificuldade;
+        }
+        public static void FacilExibir()
+        {
+            Console.WriteLine("\nDificuldade Fraca selecionada");
+            totalDeTentativas = 10;
+        }
+        public static void MedioExibir()
+        {
+            Console.WriteLine("\nDificuldade Média selecionada");
+            totalDeTentativas = 5;
+        }
+        public static void DificilExibir()
+        {
+            Console.WriteLine("\nDificuldade Difícil selecionada");
+            totalDeTentativas = 3;
+        }
+        public static void FalhaExibir()
+        {
+            Console.WriteLine("\nMeu consagrado você arrumo errado vai lá e faz denovo");
+            Console.WriteLine("Aperte ENTER para continuar");
+            Console.ReadLine();
+        }
+        public static int Gameplay(int tentativas)
+        {
+            Console.Clear();
+            Console.WriteLine("==========================");
+            Console.WriteLine($"Tentativas {tentativas} de {totalDeTentativas}");
+            Console.WriteLine("==========================");
+
+            Console.WriteLine("\nDigite um número entre 1 e 20 e veja se ganha ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            return numeroDigitado;
+
+        }
+        public static void VitoriaExibir()
+        {
+            Console.WriteLine("\n==========================");
+            Console.WriteLine("Parabéns, você acertou e vai ganhar nada (:");
+            Console.WriteLine("==========================");
+
+        }
+        public static void DerrotaExibir(int numeroSecreto)
+        {
+            Console.WriteLine("\n==========================");
+            Console.WriteLine($"Mas que peninha, parece que alguem vai sair na divida, o número era {numeroSecreto}");
+            Console.WriteLine("Mas você chegou perto de ganhar o GRANDE PRÊMIO quer tentar denovo???");
+            Console.WriteLine("==========================");
+
+        }
+        public static void NaltoExibir()
+        {
+            Console.WriteLine("\n==========================");
+            Console.WriteLine("Ta indo muito alto em!!!");
+            Console.WriteLine("==========================");
+
+        }
+        public static void NbaixoExibir()
+        {
+            Console.WriteLine("\n==========================");
+            Console.WriteLine("Ta indo muito baixo em!!!");
+            Console.WriteLine("==========================");
+
+        }
+        public static string mensagemFinal()
+        {
+            Console.WriteLine("Deseja continuar? (S/N)");
+            string opcaoContinuar = Console.ReadLine().ToUpper();
+
+            return opcaoContinuar;
         }
     }
+
 }
